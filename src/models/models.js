b895f51368a385da078122d437158bda5ff91e73
Sofia <sofia.recreio2@gmail.com>
2024-03-14 22:29:33 -0300

    feat: add modelo de usuario sem servidor

---
+++ src/models/models.js
@@ -0,0 +1,28 @@
+//Modelo de Usuário
+const User = sequelize.define("User", {
+    Userid: {
+        type: DataTypes.UUIDV4,
+        defaultValue: () => uuidv4(),
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    idade: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    nomePai: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    nomeMae: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    cpf: {
+        type: DataTypes.STRING(11),
+        allowNull: false,
+    }
+});

f5cfe155192fec9e7ffcd376ffd17aa77e91b11f
SpikDetron <euarthuroctavi@gmail.com>
2024-03-15 02:05:28 -0300

    feat/crudturmas

--- src/controllers/turmacontrollers.js
+++ src/controllers/turmacontrollers.js
@@ -12,7 +12,7 @@ async function criarTurma(req, res) {
         const turma = await Turma.create({
             Numero,
             Turno,
-            Serie,
+            Serie
             
         })
 
@@ -33,7 +33,31 @@ async function getTurma(req, res) {
     }
 }
 
+async function deleteTurma(req, res) {
+    const {id} = req.params
+    try {
+        const turma = await Turma.findOne({where: {id:id}})
+        await turma.delete()
+        return res.status(200).json(turma)
+    }
+    catch (error) {
+        return res.status(400).json({ error: "nao foi possivel deletar a turma" })
+}}
+
+async function updateTurma(req, res){
+    const {id} = req.params
+    try {
+        const turma = await Turma.findOne({where: {id:id}})
+        await turma.update(req.body)
+        return res.status(200).json(turma)
+    }
+    catch (error) {
+        return res.status(400).json({ error: "nao foi possivel deletar a turma" })
+}}
+
 module.exports = {
+    deleteTurma,
     criarTurma,
-    getTurma
+    getTurma,
+    updateTurma
 }

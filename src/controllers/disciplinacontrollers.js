457288d0d69b8d0a9e5cf19287c0abe8b3099696
SpikDetron <euarthuroctavi@gmail.com>
2024-03-19 00:31:27 -0300

    feat/deleteDisciplinas

--- src/controllers/disciplinacontrollers.js
+++ src/controllers/disciplinacontrollers.js
@@ -19,3 +19,23 @@ async function criarDisciplinas(req, res) {
     }
 }
 
+async function getDisciplina(req, res) {
+    try {
+        const disciplina = await Disciplinas.findAll()
+        return res.status(200).json(disciplina)
+
+    } catch (error) {
+        return res.status(400).json({ error: "nao foi possivel listar as disciplinas" })
+    }
+}
+async function deleteDisciplina(req, res) {
+    const {id} = req.params
+    try {
+        const disciplina = await Disciplinas.findOne({where: {id:id}})
+        await disciplina.delete()
+        return res.status(200).json(disciplina)
+    }
+    catch (error) {
+        return res.status(400).json({ error: "nao foi possivel deletar a disciplina" })
+}}
+

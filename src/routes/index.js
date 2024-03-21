3894e6594a55457081ebde9de2189d093304a74e
Weslley Ribeiro <weslleysilvaribeiro35@gmail.com>
2024-03-21 11:30:57 -0300

    feat: criacao crud coordenadores

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,6 +7,8 @@ const userRoutes = require('./user.routes');
 
 const alunosRoutes = require('./alunos.routes');
 
+const coordenadorRoutes = require('./coordenador.routes');
+
 const router = Router();
 
 router.use('/users', userRoutes);
@@ -14,5 +16,7 @@ router.use('/users', userRoutes);
 router.use("/turmas", turmasRoutes);
 
 router.use("/alunos", alunosRoutes);
+
+router.use("/coordenador", coordenadorRoutes)
 
 module.exports = router;

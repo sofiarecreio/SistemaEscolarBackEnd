7a45a065630020c1802ea778a92ddf919d4dff97
SpikDetron <euarthuroctavi@gmail.com>
2024-03-25 22:47:26 -0300

    feat/turmasRoutes

--- src/routes/turmas.routes.js
+++ src/routes/turmas.routes.js
@@ -1,14 +1,25 @@
 const express = require("express");
 
 const turmasRoutes = express.Router();
+const turmasController = require("../controllers/turmascontroller.js");
 
 //Inclusão dos Middlewares
 const checkTurmaExists = require("../middleware/checkTurmaExistsMiddleware");
 
-//Inclusão dos Controllers
-const turmasController = require("../controllers/turmasController");
 
-// Criar turmas
-turmasRoutes.post("/", checkTurmaExists, (request, response) => turmasController.criarTurma(request, response));
+
+
+//ver todos os turma
+turmasRoutes.get("/", (req,res)=> turmasController.getTurma(req,res));
+
+//Criar Usuario
+turmasRoutes.post("/", (req,res)=> turmasController.criarTurma(req,res));
+
+//Deletar usuario
+turmasRoutes.delete("/:id", checkTurmaExists, (req,res)=> turmasController.deleteTurma(req,res));
+
+//atualizar usuario
+turmasRoutes.put("/:id", checkTurmaExists, (req,res)=> turmasController.updateTurma(req,res));
+
 
 module.exports = turmasRoutes;

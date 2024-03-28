bbfe0849728e0d1547649d6153f853b35dc36e8e
SpikDetron <euarthuroctavi@gmail.com>
2024-03-28 16:56:18 -0300

    fix/rotaLogin

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,8 @@ const professorRoutes = require('./professor.routes')
 
 const coordenadorRoutes = require('./coordenador.routes');
 
+const loginRoutes = require('./login.routes');
+
 const router = Router();
 
 router.use('/users', userRoutes);
@@ -27,5 +29,8 @@ router.use("/professores", professorRoutes)
 router.use("/disciplinas", disciplinaRoutes)
 
 router.use("/coordenador", coordenadorRoutes)
+
+router.use("/login", loginRoutes)
+
 
 module.exports = router;
